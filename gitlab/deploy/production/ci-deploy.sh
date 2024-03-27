2b70650a77d3e031ce926da51cd9e6311fb41b63
German Cabarcas <gcc@unironical.com>
2024-03-27 13:32:33 -0500

    cleanup old build artifacts

--- gitlab/deploy/production/ci-deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rupgradeall
-stack-add "$CI_ENV" "$CI_PROJECT_NAMESPACE"
-waitforurl "https://${HOSTNAME}/" -t 300
-echo "Successfully deployed app to https://$HOSTNAME"
--- gitlab/deploy/staging/ci-after-deploy.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-cd /usr/src/app/ || exit 1
