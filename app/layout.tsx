9040a9d87a3c5c762a0867ebaf8853890fdf7914
German Cabarcas <gcc@unironical.com>
2024-05-22 02:46:08 -0500

    GA using Partytown test

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,12 +3,13 @@ import DesktopMenu from '@/components/DesktopMenu'
 import Footer from '@/components/Footer'
 import MobileMenu from '@/components/MobileMenu'
 import { GoogleAnalytics } from '@next/third-parties/google'
-import { Rethink_Sans } from 'next/font/google';
+import { Partytown } from '@builder.io/partytown/react'
+import { Rethink_Sans } from 'next/font/google'
 
 const rethink_sans = Rethink_Sans({
-    display: 'swap',
-    style: 'normal',
-    subsets: ['latin'],
+  display: 'swap',
+  style: 'normal',
+  subsets: ['latin']
 })
 
 const GA_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID
@@ -39,11 +40,36 @@ export default function RootLayout({
             content='TvkzPehNPl8bgENLuXZdpg5oAbt1P2Zai0mO2-wTs60'
           />
         ) : null}
+
+        <Partytown forward={['dataLayer.push']} />
+        <script
+          type='text/partytown'
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+        ></script>
+        <script
+          type='text/partytown'
+          dangerouslySetInnerHTML={{
+            __html: `
+    window.dataLayer = window.dataLayer || [];
+    window.gtag = function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', '${GA_ID}');
+    `
+          }}
+        />
       </head>
-      <body className={['flex flex-col min-h-screen overflow-x-hidden', rethink_sans.className].join(', ')}>
+      <body
+        className={[
+          'flex flex-col min-h-screen overflow-x-hidden',
+          rethink_sans.className
+        ].join(', ')}
+      >
         <DesktopMenu />
         <MobileMenu />
+        {/*
         {GA_ID ? <GoogleAnalytics gaId={GA_ID} /> : null}
+        */}
         {children}
         <Footer />
       </body>
