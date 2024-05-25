4f153d1217dc46ce773b213f85e2bd970a5a32a7
German Cabarcas <gcc@unironical.com>
2024-05-25 16:53:47 -0500

    Adds handling for redirects collection items

--- next.config.js
+++ next.config.js
@@ -2,15 +2,26 @@
 process.env.HOSTNAME = '0.0.0.0'
 const nextConfig = {
   images: {
-    domains: [
-      process.env.NEXT_PUBLIC_API_IMAGE_HOST,
-    ]
+    domains: [process.env.NEXT_PUBLIC_API_IMAGE_HOST]
   },
   output: 'standalone',
   swcMinify: true,
   eslint: {
     ignoreDuringBuilds: true,
     dirs: ['modules', 'pages', 'components']
+  },
+  async redirects() {
+    const urls = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/redirects`
+    ).then(res => res.json())
+    if (urls.data === null || urls.data.length === 0) {
+      return []
+    }
+    return urls.data.map(x => ({
+      source: x.attributes.origin_url,
+      destination: x.attributes.target_url,
+      permanent: true
+    }))
   }
 }
 
